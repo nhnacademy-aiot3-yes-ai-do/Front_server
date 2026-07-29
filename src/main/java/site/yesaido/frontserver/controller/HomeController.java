209547cf7f5a657975946d8db8087e84f4bb48f7
{seoyoung7646} <{pft7fytxvn@privaterelay.appleid.com}>
2026-07-29 10:47:34 +0900

    Todo삭제

--- src/main/java/site/yesaido/frontserver/controller/HomeController.java
+++ src/main/java/site/yesaido/frontserver/controller/HomeController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// TODO: 백엔드 연동 준비가 되면 accessToken 쿠키 유효성 검증(만료/위조 체크)을 실제로 붙여야 합니다.
-//       지금은 로그인 시 서버가 accessToken 쿠키를 내려준다는 전제로, 쿠키 존재 여부만으로
-//       로그인 상태를 판단하는 단순화된 버전입니다.
 @Controller
 public class HomeController {
 
