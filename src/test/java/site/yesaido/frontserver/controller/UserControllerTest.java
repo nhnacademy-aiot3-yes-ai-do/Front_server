e1424a3d0614d3d11b55dcde1f3f5ad06c7d2ae9
JaeUng <rnrn4308428@gmail.com>
2026-07-30 14:00:23 +0900

    fix : UserControllerTest

--- src/test/java/site/yesaido/frontserver/controller/UserControllerTest.java
+++ src/test/java/site/yesaido/frontserver/controller/UserControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import site.yesaido.frontserver.controller.user.UserController;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
