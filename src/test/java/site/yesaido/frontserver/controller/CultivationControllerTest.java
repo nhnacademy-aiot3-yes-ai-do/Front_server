af3ff174802bdb808b3269b8fb32c5afd6e49158
Poly-Etilen <wlsdud04060@naver.com>
2026-08-22 13:45:23 +0900

    fix: test 수정

--- src/test/java/site/yesaido/frontserver/controller/CultivationControllerTest.java
+++ src/test/java/site/yesaido/frontserver/controller/CultivationControllerTest.java
@@ -1,5 +1,6 @@
 package site.yesaido.frontserver.controller;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -76,6 +77,9 @@ class CultivationControllerTest {
 
     @MockitoBean
     private AiClient aiClient;
+
+    @MockitoBean
+    private MeterRegistry meterRegistry;
 
     @Test
     @DisplayName("로그인 안 한 상태로 목록 접근 시 로그인 페이지로 리다이렉트")
