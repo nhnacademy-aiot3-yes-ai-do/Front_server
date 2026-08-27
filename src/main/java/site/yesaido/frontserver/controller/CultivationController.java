bcb7db132e2b398538dcf86c5b6c55918a8526d6
교동 <tjdud0309106@gmail.com>
2026-08-27 16:58:52 +0900

    재배지이름 valid 추

--- src/main/java/site/yesaido/frontserver/controller/CultivationController.java
+++ src/main/java/site/yesaido/frontserver/controller/CultivationController.java
@@ -1,13 +1,17 @@
 package site.yesaido.frontserver.controller;
 
 import feign.FeignException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import site.yesaido.frontserver.client.AiClient;
 import site.yesaido.frontserver.client.CultivationClient;
 import site.yesaido.frontserver.client.SensorClient;
@@ -124,8 +128,16 @@ public class CultivationController {
     }
   
     @PostMapping
-    public String createCultivation(@RequestParam String name, @RequestParam Long mushroomId) {
-        cultivationClient.createCultivation(new CultivationCreateRequest(name, mushroomId));
+    public String createCultivation(@Valid @ModelAttribute CultivationCreateRequest request,
+                                    BindingResult bindingResult,
+                                    RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            FieldError fieldError = bindingResult.getFieldError();
+            String message = fieldError != null ? fieldError.getDefaultMessage() : "입력값을 다시 확인해주세요.";
+            redirectAttributes.addFlashAttribute("cultivationCreateError", message);
+            return "redirect:/cultivations/new";
+        }
+        cultivationClient.createCultivation(request);
         return "redirect:/cultivations";
     }
 
